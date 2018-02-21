1096bab9f4b5a580d6537e2641c6520aeb8ba0a6
Whitney Ford <whitneyforddev@gmail.com>
2018-02-21 14:23:33 -0500

    Added outbund property

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -21,6 +22,14 @@ public class HelloMavenTest extends FunctionalTestCase {
     protected String [] getConfigFiles() {
     	String [] files = {"maven-project.xml", "global.xml"};
         return files;
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {  	
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+      System.out.println("\n\nTestcase-2 dynamic port ----------->" + myPort.getNumber());
     }
 
 }
